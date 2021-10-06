b771c83ad2bb3836b8da1a7a9a0e9565f13f088a
Luis Carrion <lcarrionalvarez@gmail.com>
2021-10-05 21:49:02 -0500

    socket emit events

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import io from 'socket.io-client';
 import { BandAdd } from './components/BandAdd';
 import { BandList } from './components/BandList';
 
+const connectSocketServer = () => {
+  const socket = io.connect('http://localhost:8080', {
+    transports: ['websocket']
+  });
+  return socket;
+}
+
 export const App = () => {
+
+  const [socket] = useState( connectSocketServer )
+  const [online, setOnline] = useState(false)
+  const [bands, setBands] = useState([])
+
+  useEffect(() => {
+    setOnline( socket.connected )
+  }, [socket])
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setOnline( true )
+    })
+  }, [socket])
+  
+  useEffect(() => {
+    socket.on('disconnect', () => {
+      setOnline( false )
+    })
+  }, [socket])
+
+  useEffect(() => {
+    socket.on('current-bands', (bands) => {
+      setBands(bands)
+    })
+  },[ socket ])
+
+  const votar = (id) => {
+    socket.emit('votar-banda', id)
+  }
+  
+  // Borrar bandas
+  const borrarBanda = (id) => {
+    socket.emit('borrar-banda', id)
+  }
+
+  // Cambiar nombre
+   const cambiarNombre = (id, nombre) => {   
+    socket.emit('cambiar-nombre-banda', {id, nombre})
+   }
+
   return (
     <div className="container">
       <div className="alert">
         <p>
           Service Status:
-          <span className="text-success"> OnLine</span>
-          <span className="text-danger"> OffLine</span>
+          {
+            online          
+             ? <span className="text-success"> OnLine</span>
+             : <span className="text-danger"> OffLine</span>
+          }          
         </p>
       </div>
       <h1>BandNames</h1>
       <hr />
       <div className="row">
         <div className="col-8">
-          <BandList />
+          <BandList
+            data = {bands}
+            votar = {votar}
+            borrar = {borrarBanda}
+            cambiarNombre = {cambiarNombre}
+          />
         </div>
         <div className="col-4">
           <BandAdd />
